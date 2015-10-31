a3488408f5c74ac0a477277936c17a739a35ac6f
Kevin Lawrence <kevinlawrence@outlook.com>
2015-10-30 22:23:41 -0700

    rotation works

--- src/spaceteroids/Space.java
+++ src/spaceteroids/Space.java
@@ -8,9 +8,11 @@ package spaceteroids;
 import environment.Environment;
 import images.ResourceTools;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.geom.AffineTransform;
 
 /**
  *
@@ -20,10 +22,16 @@ class Space extends Environment {
 
     Image ship;
     int angle = 0;
+    int shipX = 400;
+    int shipY = 300;
+    
+    private double getAngleInRadians(){
+        return Math.toRadians(angle);
+    }
     
     public Space() {
         setBackground(ResourceTools.loadImageFromResource("spaceteroids/color_space_cloud.jpg"));
-        ship = ResourceTools.loadImageFromResource("spaceteroids/ship.png");
+        ship = ResourceTools.loadImageFromResource("spaceteroids/ship.png").getScaledInstance(100, 200, Image.SCALE_SMOOTH);
     }
 
     @Override
@@ -38,12 +46,12 @@ class Space extends Environment {
 
     @Override
     public void keyPressedHandler(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
             angle = (++angle % 360);
-        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             angle = (--angle % 360);
         }
-        System.out.println("Angle " + angle);
+//        System.out.println("Angle " + angle);
     }
     
 
@@ -59,9 +67,19 @@ class Space extends Environment {
 
     @Override
     public void paintEnvironment(Graphics graphics) {
+
+        Graphics2D g2d = (Graphics2D) graphics;
+        AffineTransform olde = g2d.getTransform();
+        
         if (ship != null) {
-            graphics.drawImage(ship, 400, 400, 100, 100, this);
+            AffineTransform at = AffineTransform.getRotateInstance(Math.toRadians(angle));
+            at.setToRotation(getAngleInRadians(), shipX + (ship.getWidth(this)/ 2), shipY + (ship.getHeight(this) / 2));
+            g2d.setTransform(at);
+            g2d.drawImage(ship, shipX, shipY, this);
         }
+        
+        g2d.setTransform(olde);
+        g2d.dispose();
     }
     
 }
